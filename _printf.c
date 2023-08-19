16fe53e61d2031e6721724da1d56e815f716aefb
engfatmait <eng_fatma_it@yahoo.com>
2023-08-19 07:50:26 -0700

    first commit of function _printf

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _printf - function that produces output according to a format.
+ * @format: a character string
+ * Return: integer (num of characters printed)
+*/
+int _printf(const char *format, ...)
+{
+	int num_arg = 0, char *str, char a;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			num_arg++;
+		}
+		else
+		{
+			format++;
+			switch (*format)
+			{
+			case '\0':
+				break;
+			case 'c':
+				{
+					a = va_arg(args, int);
+					write(1, &a, 1);
+					num_arg++;
+				}
+			case 's':
+				{
+					str = va_arg(args, char*);
+					write(1, str, strlen(str)), num_arg += strlen(str);
+				}
+			case '%':
+				{
+					write(1, format, 1), num_arg++;
+				}
+			}
+		}
+		format++;
+	}
+	va_end(args), return (num_arg);
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+int _printf(const char *format, ...);
+
+
+
+
+
+#endif
