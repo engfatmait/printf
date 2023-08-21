2f170efd255b3f5efdc4d71d7fa8536db0bf4e12
engfatmait <eng_fatma_it@yahoo.com>
2023-08-20 21:08:18 -0700

    try to handle null cases

--- _printf.c
+++ _printf.c
@@ -10,10 +10,10 @@ int _printf(const char *format, ...)
 	char *str;
 	char a;
 	va_list args;
-
-	if (!format)
-		return (-1);
+	
 	va_start(args, format);
+	if (!format || format[0] == '%' && format[1] == '' && !format[2])
+		return (-1);
 	while (*format)
 	{
 		if (*format != '%')
